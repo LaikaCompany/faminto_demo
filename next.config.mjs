235e05f8ab7f07044e527c31d04d9588554fb181
LaikaCompany <laikacompany.tech@gmail.com>
2024-11-19 23:36:35 +0100

    deploy 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath:"/faminto_demo",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    basePath:'/faminto_demo',
     experimental: {
         missingSuspenseWithCSRBailout: false,
       },
