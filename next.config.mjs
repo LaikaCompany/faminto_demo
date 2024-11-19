31337856e653e24a0ee973b90494d5c639caa89a
LaikaCompany <laikacompany.tech@gmail.com>
2024-11-19 23:21:43 +0100

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
 };
+
 
 export default nextConfig;
